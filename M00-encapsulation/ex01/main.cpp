ad05ec868b564d240562c175365b4f864e0f64bd
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-09-12 22:35:38 -0300

    M00 - ex01 - file tests

--- M00-encapsulation/ex01/main.cpp
+++ M00-encapsulation/ex01/main.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 
 #include "Graph.hpp"
+#include "test_utils.hpp"
 
 int main(void) {
     Graph graph = Graph();
 
-    std::cout << "Empty graph:" << std::endl;
+    test::subtitle("Empty graph:");
     std::cout << graph << std::endl << std::endl;
 
+    test::subtitle("Adding point (0,0):");
     graph.addPoint(0, 0);
-    std::cout << "With point(0,0):" << std::endl;
     std::cout << graph << std::endl << std::endl;
 
     graph.addPoint(2, 2);
     graph.addPoint(4, 2);
     graph.addPoint(2, 4);
 
-    std::cout << "PDF example:" << std::endl;
+    test::subtitle("PDF example:");
     std::cout << graph << std::endl << std::endl;
 
     graph.addPoint(2, 8);
@@ -29,17 +30,50 @@ int main(void) {
     graph.addPoint(7, 7);
     graph.addPoint(8, 8);
     graph.addPoint(9, 9);
-    std::cout << "More points:" << std::endl;
+    test::subtitle("More points:");
     std::cout << graph << std::endl << std::endl;
 
     graph.addPoint(10, 10);
     graph.addPoint(20, 20);
     graph.addPoint(100, 100);
-    std::cout << "Bigger:" << std::endl;
-    std::cout << graph << std::endl;
+    test::subtitle("Bigger:");
+    std::cout << graph << std::endl << std::endl;
 
     graph.addPoint(6.4, 1.6);  // rounds to 6, 2
     graph.addPoint(2.1, 5.5);  // rounds to 2, 6
-    std::cout << "With some floats:" << std::endl;
-    std::cout << graph << std::endl;
+    test::subtitle("With some floats:");
+    std::cout << graph << std::endl << std::endl;
+
+    test::title("FILE TESTS");
+    test::subtitle("Reading from unexistent file:");
+    try {
+        Graph graphFile = Graph("wrong_name");
+    } catch (std::exception &e) {
+        test::error(e.what());
+    }
+
+    test::subtitle("File with wrong input format:");
+    try {
+        Graph graphFile = Graph("./sample/error");
+        std::cout << graphFile << std::endl << std::endl;
+    } catch (std::exception &e) {
+        test::error(e.what());
+    }
+
+    test::subtitle("Reading from empty file:");
+    try {
+        Graph graphFile = Graph("./sample/empty");
+        std::cout << graphFile << std::endl << std::endl;
+    } catch (std::exception &e) {
+        test::error(e.what());
+    }
+
+    test::subtitle("Reading from a file:");
+    try {
+        Graph graphFile = Graph("./sample/points");
+        std::cout << graphFile << std::endl << std::endl;
+    } catch (std::exception &e) {
+        test::error(e.what());
+    }
+
 }
